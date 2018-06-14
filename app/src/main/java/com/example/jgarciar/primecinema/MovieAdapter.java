d88ee71fe23481f3a425bd23f87e2e4a4104a12d
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2018-06-14 15:25:05 -0500

    Add posters to cards and make minor UI formatting changes.

--- app/src/main/java/com/example/jgarciar/primecinema/MovieAdapter.java
+++ app/src/main/java/com/example/jgarciar/primecinema/MovieAdapter.java
@@ -7,10 +7,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>
 {
+
+    private final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/w185/";
 
     private List<Movie> dataList;
 
@@ -30,23 +35,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position)
     {
-        holder.mMovieRating.setText(dataList.get(position).getVoteAverage().toString());
+        //holder.mMovieRating.setText(dataList.get(position).getVoteAverage().toString());
         holder.mMovieTitle.setText(dataList.get(position).getTitle());
         holder.mMovieOverview.setText(dataList.get(position).getOverview());
+
+        String posterUrl = BASE_POSTER_URL + dataList.get(position).getPosterPath();
+
+        Glide.with(holder.itemView.getContext())
+                .load(posterUrl)
+                .apply(RequestOptions.fitCenterTransform())
+                .into(holder.mMoviePoster);
     }
 
     class MovieViewHolder extends RecyclerView.ViewHolder
     {
-        TextView mMovieRating;
+        //TextView mMovieRating;
         TextView mMovieTitle;
         TextView mMovieOverview;
+        ImageView mMoviePoster;
 
         MovieViewHolder(View itemView)
         {
             super(itemView);
-            mMovieRating = itemView.findViewById(R.id.tv_movie_rating);
+            //mMovieRating = itemView.findViewById(R.id.tv_movie_rating);
             mMovieTitle = itemView.findViewById(R.id.tv_movie_title);
             mMovieOverview = itemView.findViewById(R.id.tv_movie_overview);
+            mMoviePoster = itemView.findViewById(R.id.iv_movie_poster);
         }
     }
 
