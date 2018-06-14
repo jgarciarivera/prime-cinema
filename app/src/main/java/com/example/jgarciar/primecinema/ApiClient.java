24710a4958bdbbf4b65b75229960d1d977b1c815
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2018-06-13 22:21:06 -0500

    Add API service client interface, Movie model class, and dependencies to Gradle build file.

---
+++ app/src/main/java/com/example/jgarciar/primecinema/ApiClient.java
@@ -0,0 +1,19 @@
+package com.example.jgarciar.primecinema;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiClient
+{
+    String BASE_URL = "https://api.themoviedb.org";
+
+    String API_KEY = "1878c131f288c61ec2c2e13e7819146d";
+
+    @GET("/3/discover/movie")
+    Call<List<Movie>> getMovies(@Query("api_key") String apiKey,
+                                @Query("with_genres") String genre,
+                                @Query("primary_release_year") String year);
+}
