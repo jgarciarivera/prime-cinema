c1fa5641eb5ed1e136db8fdfd95becc077350866
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2018-06-15 11:05:35 -0500

    Modify TextView padding, increase card size, and add placeholder for director and mpaa rating textviews.

--- app/src/main/java/com/example/jgarciar/primecinema/MovieAdapter.java
+++ app/src/main/java/com/example/jgarciar/primecinema/MovieAdapter.java
@@ -43,7 +43,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
         Glide.with(holder.itemView.getContext())
                 .load(posterUrl)
-                .apply(RequestOptions.overrideOf(600,600))
+                .apply(RequestOptions.overrideOf(0,0))
                 .apply(RequestOptions.fitCenterTransform())
                 .into(holder.mMoviePoster);
     }
