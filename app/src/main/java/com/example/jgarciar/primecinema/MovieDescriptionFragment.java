4bb37008a01e8ced8bb3f368173df5d6cde6ac5b
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2018-06-18 10:20:09 -0500

    Format cards

--- app/src/main/java/com/example/jgarciar/primecinema/MovieDescriptionFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.jgarciar.primecinema;
-
-public class MovieDescriptionFragment {
-}
---
+++ app/src/main/java/com/example/jgarciar/primecinema/MovieDetailsFragment.java
@@ -0,0 +1,16 @@
+package com.example.jgarciar.primecinema;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieDetailsFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
